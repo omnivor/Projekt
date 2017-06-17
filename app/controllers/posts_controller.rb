0608e7bd1851d341dbec0ed53682ae7c2fc0a356
omnivor <lukasz.targanski@gmail.com>
2017-06-17 12:46:38 +0200

    nick

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,7 +71,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:description, :image)
+      params.require(:post).permit(:description, :image, :name)
     end
     def correct_user
       @post = current_user.posts.find_by(id: params[:id])
